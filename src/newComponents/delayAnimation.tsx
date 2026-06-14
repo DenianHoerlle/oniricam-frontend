2060adfd0e41c02463d77ebac417132ca2739f64
DenianHoerlle <deniancloss@gmail.com>
2026-06-14 14:31:25 -0300

    refact delay animation component

--- src/newComponents/delayAnimation.tsx
+++ src/newComponents/delayAnimation.tsx
@@ -1,21 +1,62 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-type DelayAnimationProps = { children: React.ReactNode; delay: number };
+type DelayAnimationProps = {
+  children: React.ReactNode;
+  delay: number;
+  className?: string;
+};
 
 const DelayAnimation = (props: DelayAnimationProps): React.ReactNode => {
-  const [shouldShow, setShouldShow] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-  const { children, delay } = props;
+  console.log("isVisible", isVisible);
 
   useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      { threshold: 0.5 },
+    );
+
+    const currentElement = ref.current;
+
+    if (currentElement) {
+      observer.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        observer.unobserve(currentElement);
+      }
+    };
+  }, []);
+
+  const [shouldShow, setShouldShow] = useState(false);
+
+  const { children, className, delay } = props;
+
+  useEffect(() => {
+    if (!isVisible) return;
+
     const animationDelay = setTimeout(() => setShouldShow(true), delay);
 
     return () => clearTimeout(animationDelay);
-  }, [delay]);
+  }, [delay, isVisible]);
 
-  const wrapperClassNames = shouldShow ? "animate-appear" : "opacity-0";
+  const wrapperClassNames = shouldShow
+    ? `w-full ${className} animate-appear`
+    : "w-full opacity-0";
 
-  return <div className={wrapperClassNames}>{children}</div>;
+  return (
+    <div ref={ref} className={wrapperClassNames}>
+      {children}
+    </div>
+  );
 };
 
 export { DelayAnimation };
