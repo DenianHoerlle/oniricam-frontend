4ebba0260fe813a2f4462692c9bacb2e735f37ad
DenianHoerlle <deniancloss@gmail.com>
2026-06-14 18:04:28 -0300

    adjust home padding

--- src/newComponents/hero.tsx
+++ src/newComponents/hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
     <DelayAnimation delay={500}>
       <div
         id="home"
-        className="flex flex-col lg:flex-row items-center gap-10 max-w-7xl mx-auto py-10 px-4 sm:px-6"
+        className="flex flex-col lg:flex-row items-center gap-10 max-w-7xl mx-auto py-20 px-4 sm:px-6"
       >
         <div className="flex flex-col gap-4 animate-appear max-w-2xl">
           <h1 className="text-4xl sm:text-5xl lg:text-7xl font-semibold leading-tight">
