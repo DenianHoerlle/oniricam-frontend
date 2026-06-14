e4facc885579bc11a8e1079ab43e63b87261e449
DenianHoerlle <deniancloss@gmail.com>
2026-06-14 17:55:03 -0300

    remove old elements

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { BenefitsSection } from "./benefits";
-export { default as Header } from "./header";
-export { HeroSection } from "./hero";
-export { InfoSection } from "./info";
-export { ProductSection } from "./product";
